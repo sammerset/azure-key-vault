c0a2439ba503ec0319404ef99d115216a94ef5f4
Mike Scott <mike.scott2@hiscox.com>
2018-01-02 11:53:49 +0000

    Merged in add_uri_encoding (pull request #4) Add URI encoding

--- spec/key_vault/version_spec.rb
+++ spec/key_vault/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'KeyVault::VERSION' do
   it 'gets the correct version' do 
-    expect(KeyVault::VERSION).to eq '0.0.9'
+    expect(KeyVault::VERSION).to eq '0.0.10'
   end
 end
